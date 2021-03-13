d48498c387ee54112d1f6bbeb252db095d946de4
Auxes1s <muignacio@up.edu.ph>
2021-03-13 22:07:34 +0800

    Finished Exercise 2

--- Exercise 2/Exercise 2.R
+++ Exercise 2/Exercise 2.R
@@ -1,4 +1,9 @@
-read_fwf(file = here("Exercise 2/Clinical Data.txt"),
+#Requisites
+library(readr)
+library(here)
+
+#Reading the .txt file
+df <- read_fwf(file = here("Exercise 2/Clinical Data.txt"),
          col_positions = fwf_cols(ID = c(1, 3),
                                   Gender = 4,
                                   DoB = c(5, 10),
@@ -11,7 +16,7 @@ read_fwf(file = here("Exercise 2/Clinical Data.txt"),
                                   vitamin = 30,
                                   preg = 31),
           col_types = cols(ID = col_character(),
-                           Gender = col_character(),
+                           Gender = col_factor(),
                            DoB = col_date(format = "%m%d%y"),
                            DoV = col_date(format = "%m%d%y"),
                            pdx = col_character(),
@@ -24,4 +29,13 @@ read_fwf(file = here("Exercise 2/Clinical Data.txt"),
          na = "."
          )
 
+#Formatting the table
+df['pdx'] <- factor(df$pdx, levels = c("01", "02", "03", "04", "05", "06", "07"), 
+                    labels = c("Cold", "Flu", "Routine Physical", "Ear Ache", "Abdominal Pain", "Heart Problem", "Lung Disorder"))
+df['sdx'] <- factor(df$sdx, levels = c("01", "02", "03", "04", "05", "06", "07"), 
+                    labels = c("Cold", "Flu", "Routine Physical", "Ear Ache", "Abdominal Pain", "Heart Problem", "Lung Disorder"))
+df['vitamin'] <- factor(df$vitamin, levels = c("1", "0"), labels = c("Yes", "No"))
+df['preg'] <- factor(df$preg, levels = c("1", "0"), labels = c("Yes", "No"))
 
+#Writing to an RDS file
+write_rds(df, "Exercise#2_Output.rds")
